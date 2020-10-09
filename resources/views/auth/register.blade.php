e5552efeacfcf84325266f1aed8c7f0a569f4f1d
misako.kikugawa <pandachooon@gmail.com>
2020-10-09 05:58:08 +0000

    user login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,7 +20,7 @@
                 </div>
                 
                 <div class="form-group">
-                    {!! Form::label('password','Password) !!}
+                    {!! Form::label('password','Password') !!}
                     {!! Form::password('password',['class' => 'form-control']) !!}
                 </div>
                 
