dd4b15c656a152129ab1c5c238d815db10d05030
misako.kikugawa <pandachooon@gmail.com>
2020-10-18 04:58:00 +0000

    timeline

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -26,7 +26,6 @@ class UsersController extends Controller
          
          // 関係するモデルの件数をロード
          $user->loadRelationshipCounts();
-            $this->loadCount(['microposts','followings','followers']);
          
          // ユーザの投稿一覧を作成日時の降順で取得
          $microposts = $user->microposts()->orderBy('created_at','desc')->paginate(10);
